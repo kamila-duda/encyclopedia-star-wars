ad7c8c1fc301a1b6d38925011d5d021835c8b154
Kamila Duda <kakus86@o2.pl>
2021-02-08 21:38:49 +0100

    added Container component

---
+++ src/common/other/Container/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledContainer } from "./styled";
+
+const Container = (props) => {
+    return (
+        <StyledContainer>
+            {props.children}
+        </StyledContainer>
+    )
+};
+
+export default Container;
