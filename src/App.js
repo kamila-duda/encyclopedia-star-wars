2ef00eae2bc59f20b394876f4fd639b04a7f8f43
Kamila Duda <kakus86@o2.pl>
2021-02-08 21:41:18 +0100

    added some components to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import HomePage from './features/HomePage';
+import { StyledHeader } from './styled';
 
 function App() {
   return (
    <>
-   test
+   <StyledHeader>
+   <h1>Welcome to The Star Wars Encyclopedia</h1>
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tellus erat, efficitur sed posuere id, feugiat id orci. Curabitur bibendum libero in gravida ultrices. Duis eu odio sit amet lorem dignissim dignissim ac in orci. Cras sit amet cursus enim. Morbi eu elit eu erat facilisis hendrerit.
+   </p>
+   </StyledHeader>
+   <HomePage/>
    </>
   );
 }
---
+++ src/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+max-width: 1200px;
+margin: auto;
+text-align: center;
+`;
