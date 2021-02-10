1c8d4ca3b21fcb528a44cd9f28136c120f7eb88e
Kamila Duda <kakus86@o2.pl>
2021-02-10 19:14:11 +0100

    new funcion to fetch details data from API

--- src/features/fetchFromAPI.js
+++ src/features/fetchFromAPI.js
@@ -1,17 +1,29 @@
 const API_URL = "https://swapi.dev/api/";
 
 export const fetchFromAPI = async ({ path }) => {
- try {
-        
-  const response = await fetch(`${API_URL}${path}`);
-  if (!response.ok) {
-    throw new Error(response.statusText);
+  try {
+    const response = await fetch(`${API_URL}${path}`);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("ups");
   }
-  
-  const configurationFile = await response.json();
-  return configurationFile;
- }
- catch (error) {
-     console.error("ups");
- }
+};
+
+export const fetchFromAPIdetail = async (path) => {
+  try {
+    const response = await fetch(path);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("ups");
+  }
 };
