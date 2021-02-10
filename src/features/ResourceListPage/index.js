201d344f196f37546f95b36927430f7d195c12c4
Kamila Duda <kakus86@o2.pl>
2021-02-10 19:14:53 +0100

    added useParams()

--- src/features/ResourceListPage/index.js
+++ src/features/ResourceListPage/index.js
@@ -3,31 +3,38 @@ import { useDispatch, useSelector } from "react-redux";
 import ListContainer from "../../common/ListContainer";
 import Tile from "../../common/Tile";
 import Container from "../../common/Container";
-import { selectResourceContentList, selectResourceContentStatus, selectResourceResults } from "./resourceListSlice";
+import {
+  fetchResourceContent,
+  selectResourceContentStatus,
+  selectResourceResults,
+} from "./resourceListSlice";
 import { StyledLink, StyledTitle } from "./styled";
+import { toDetailsPage } from "../../core/routes";
+import { useParams } from "react-router-dom";
 
 const ResourceListPage = () => {
- // const dispatch = useDispatch();
- const status = useSelector(selectResourceContentStatus);
- 
- const results = useSelector(selectResourceResults)
-const checkResource = useSelector(selectResourceContentList);
-
+  const dispatch = useDispatch();
+  const status = useSelector(selectResourceContentStatus);
+  const results = useSelector(selectResourceResults);
+  const { resource } = useParams();
+  useEffect(() => {
+    dispatch(fetchResourceContent(resource));
+  }, []);
   return (
     <>
       {status === "loading" ? (
         <p>wait</p>
       ) : (
         <>
-            <StyledTitle>{checkResource}</StyledTitle>
-            <Container>
+          <StyledTitle>{resource}</StyledTitle>
+          <Container>
             <ListContainer home={false}>
-              {results.map((key) => (
+              {results.map((key, index) => (
                 <StyledLink
-                  key={key.name? key.name : key.title}
-                 
+                  key={key.name ? key.name : key.title}
+                  to={toDetailsPage({ resource: resource, id: index + 1 })}
                 >
-                  <Tile title={key.name? key.name : key.title} />
+                  <Tile title={key.name ? key.name : key.title} />
                 </StyledLink>
               ))}
             </ListContainer>
