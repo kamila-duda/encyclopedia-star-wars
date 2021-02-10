b635b0f5c624afe7c26c1d528f9ce40649056f38
Kamila Duda <kakus86@o2.pl>
2021-02-10 19:15:42 +0100

    formatted code

--- src/features/HomePage/homeSaga.js
+++ src/features/HomePage/homeSaga.js
@@ -9,7 +9,9 @@ import { fetchFromAPI } from "../fetchFromAPI";
 function* fetchResourceHandler() {
   try {
     yield delay(500);
-    const resource = yield call(fetchFromAPI, { path: "" });
+    const resource = yield call(fetchFromAPI, {
+      path: "",
+    });
     yield put(fetchResourceSuccess(resource));
   } catch (error) {
     yield put(fetchResourceError());
