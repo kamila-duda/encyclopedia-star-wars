11fc7d1c10ff59fe1df2833e9a47f36a94870d9d
Kamila Duda <kakus86@o2.pl>
2021-02-10 19:12:57 +0100

    added new saga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,7 +1,8 @@
 import { watchFetchResource } from "../features/HomePage/homeSaga";
 import { all } from "redux-saga/effects";
 import { watchFetchList } from "../features/ResourceListPage/resourceListSaga";
+import { watchFetchDetails } from "../features/DetailsPage/detailsSaga";
 
 export default function* rootSaga() {
-  yield all([watchFetchResource(), watchFetchList()]);
+  yield all([watchFetchResource(), watchFetchList(), watchFetchDetails()]);
 }
