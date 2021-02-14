f1102522fd7111843f2540f320449bdf280e7ebf
Kamila Duda <kakus86@o2.pl>
2021-02-14 13:56:41 +0100

    styled grid for mobile

--- src/common/ListContainer/styled.js
+++ src/common/ListContainer/styled.js
@@ -14,9 +14,12 @@ export const StyledListContainer = styled.div`
     css`
       grid-template-columns: repeat(3, 1fr);
       grid-gap: 20px;
-
+      grid-auto-rows: 150px;
       @media (max-width: ${({ theme }) => theme.media.tablet}) {
         grid-template-columns: repeat(2, 1fr);
+      }
+      @media (max-width: ${({ theme }) => theme.media.extraMobile}) {
+        grid-auto-rows: unset;
       }
     `}
 `;
